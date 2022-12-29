dfabf2277e97abd2084e52b027990243898bc99c
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-29 13:34:27 +0700

    feat: add listener on connection and disconnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
+import {Socket} from "socket.io";
+
 const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
+const socketio = require('socket.io');
 const router = require('./router');
 
 dotenv.config();
@@ -8,8 +11,17 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(router);
 const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log(`We have a new connection ${socket}`);
+  socket.on("disconnect", () => {
+    console.log("User have been disconnected...");
+  })
+})
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
