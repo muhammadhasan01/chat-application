5f0bbebb2ecfe1cd9ef3a347c279e38ef7f1a09c
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-29 13:29:31 +0700

    refactor: make `src` folder for source code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
 const express = require('express');
-const socketio = require('socket.io');
 const http = require('http');
 const dotenv = require('dotenv');
+const router = require('./router');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(router);
 const server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
-
+export {};
