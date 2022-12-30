e7217efc8d3da3d0f7567eacbc2c086819e83e55
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-30 12:52:56 +0700

    refactor: move `Chat.tsx` to its own folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Join from "./components/Join/Join";
-import Chat from "./components/Chat";
+import Chat from "./components/Chat/Chat";
 import "./App.css";
 
 const App = () => (
