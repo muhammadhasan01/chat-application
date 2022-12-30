4b6287fa6ef756f0025ff24ef14a16c2c48614d7
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-30 20:48:44 +0700

    fix: add CORS into socket io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import {Server, Socket} from "socket.io";
 import express from "express";
+import cors from "cors";
 import {createServer} from "http";
 import router from "./router";
 import dotenv from "dotenv";
@@ -9,8 +10,15 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 io.on("connection", (socket: Socket) => {
   console.log(`We have a new connection ${socket}`);
@@ -22,5 +30,3 @@ io.on("connection", (socket: Socket) => {
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
-
-export {};
