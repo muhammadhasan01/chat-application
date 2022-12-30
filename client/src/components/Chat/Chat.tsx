16c75b379e873f23b271d4054c2f1f4a8a58e327
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-30 20:49:25 +0700

    feat: add initial Chat component

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,8 +1,52 @@
+import {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import {io} from 'socket.io-client';
+import {useLocation} from "react-router";
+import {ChatQueryString} from "../../helper/interfaces";
+
+const socket = io(import.meta.env.VITE_SERVER_ENDPOINT, {
+  withCredentials: true,
+  transports: ['websocket']
+});
+
 const Chat = () => {
+  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+  const [lastPong, setLastPong] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
+  const [room, setRoom] = useState<string | null>(null);
+
+  const {search} = useLocation();
+
+  useEffect(() => {
+    const {name, room} = queryString.parse(search) as ChatQueryString;
+    setName(name);
+    setRoom(room);
+  }, []);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
   return (
-    <div>
+    <h1>
       Chat
-    </div>
+    </h1>
   )
 }
 
