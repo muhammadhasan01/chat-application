34efccb33ee715e37dad76b61a81ecb6143be63b
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-12-31 04:18:56 +0700

    refactor: use simpler socket init

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,47 +1,36 @@
 import {useState, useEffect} from 'react';
 import queryString from 'query-string';
-import {io} from 'socket.io-client';
+import {io, Socket} from 'socket.io-client';
 import {useLocation} from "react-router";
 import {ChatQueryString} from "../../helper/interfaces";
 
-const socket = io(import.meta.env.VITE_SERVER_ENDPOINT, {
-  withCredentials: true,
-  transports: ['websocket']
-});
+let socket: Socket;
 
 const Chat = () => {
-  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
-  const [lastPong, setLastPong] = useState<string | null>(null);
   const [name, setName] = useState<string | null>(null);
   const [room, setRoom] = useState<string | null>(null);
 
+  const endpoint = import.meta.env.VITE_SERVER_ENDPOINT;
   const {search} = useLocation();
 
   useEffect(() => {
     const {name, room} = queryString.parse(search) as ChatQueryString;
+
     setName(name);
     setRoom(room);
-  }, []);
 
-  useEffect(() => {
-    socket.on('connect', () => {
-      setIsConnected(true);
+    socket = io(endpoint, {
+      withCredentials: true,
+      transports: ['websocket']
     });
 
-    socket.on('disconnect', () => {
-      setIsConnected(false);
-    });
-
-    socket.on('pong', () => {
-      setLastPong(new Date().toISOString());
-    });
+    socket.emit('join', {name, room});
 
     return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('pong');
-    };
-  }, []);
+      socket.emit("disconnect");
+      socket.off();
+    }
+  }, [search, endpoint]);
 
   return (
     <h1>
