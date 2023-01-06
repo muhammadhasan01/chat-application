042c6ee7ba4fc710723aec027e9b351e6c19d1a8
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-06 20:15:46 +0700

    feat: add initial socket send message in client

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -3,12 +3,15 @@ import queryString from 'query-string';
 import {io, Socket} from 'socket.io-client';
 import {useLocation} from "react-router";
 import {ChatQueryString} from "../../helper/interfaces";
+import {ChatMessage} from "../../../../server/src/helper/interfaces";
 
 let socket: Socket;
 
 const Chat = () => {
   const [name, setName] = useState<string | null>(null);
   const [room, setRoom] = useState<string | null>(null);
+  const [message, setMessage] = useState<ChatMessage | null>(null);
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   const endpoint = import.meta.env.VITE_SERVER_ENDPOINT;
   const {search} = useLocation();
@@ -24,7 +27,9 @@ const Chat = () => {
       transports: ['websocket']
     });
 
-    socket.emit('join', {name, room});
+    socket.emit('join', {name, room}, () => {
+
+    });
 
     return () => {
       socket.emit("disconnect");
@@ -32,10 +37,37 @@ const Chat = () => {
     }
   }, [search, endpoint]);
 
+  useEffect(() => {
+    socket.on('message', (message: ChatMessage) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages])
+
+  const sendMessage = (e: React.KeyboardEvent) => {
+    e.preventDefault();
+    if (!message) {
+      return;
+    }
+    socket.emit("sendMessage", message, () => setMessage(null));
+  };
+
+  if (!message) {
+    return <div>Loading...</div>;
+  }
+
+  console.log({name, room});
+  console.log({message, messages});
+
   return (
-    <h1>
-      Chat
-    </h1>
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message.text}
+          onChange={e => setMessage({name: message.name, text: e.target.value})}
+          onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+        />
+      </div>
+    </div>
   )
 }
 
