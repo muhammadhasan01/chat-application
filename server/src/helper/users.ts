db7c498a93ef2b984109249bdaba8761f73e6dcd
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-06 20:13:42 +0700

    feat: add `users.ts`

---
+++ server/src/helper/users.ts
@@ -0,0 +1,30 @@
+import {CreateUserResponse, UserModel} from "./interfaces";
+
+const users: UserModel[] = [];
+
+const addUser = ({id, name, room}: UserModel): CreateUserResponse => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find(user => user.name === name && user.room === room);
+  if (existingUser) {
+    return {error: 'Username is already taken'};
+  }
+  const user: UserModel = {id, name, room};
+  users.push(user);
+  return {user};
+}
+
+const removeUser = (id: string) => {
+  const index = users.findIndex(user => user.id === id);
+  if (index === -1) {
+    return {};
+  }
+  return users.splice(index, 1)[0];
+}
+
+const getUser = (id: string) => users.find(user => user.id === id);
+
+const getUsersInRoom = (room: string) => users.filter(user => user.room === room);
+
+export {addUser, removeUser, getUser, getUsersInRoom};
