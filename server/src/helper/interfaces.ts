fa9f767553cf60c09702f267550bc4b73e58d135
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-06 20:13:55 +0700

    feat: add more interfaces

--- server/src/helper/interfaces.ts
+++ server/src/helper/interfaces.ts
@@ -2,3 +2,19 @@ export interface ChatProp {
   room: string
   name: string
 }
+
+export interface UserModel {
+  id: string
+  name: string
+  room: string
+}
+
+export interface CreateUserResponse {
+  error?: string
+  user?: UserModel
+}
+
+export interface ChatMessage {
+  name: string
+  text: string
+}
