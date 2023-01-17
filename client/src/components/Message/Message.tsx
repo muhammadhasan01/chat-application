6b1d7366144469775bfe8f4d0aa3c004bffa8fc3
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-17 19:38:35 +0700

    feat: add `Messages.tsx` component

---
+++ client/src/components/Message/Message.tsx
@@ -0,0 +1,25 @@
+import "./Message.css";
+import {MessageProps} from "../../helper/interfaces";
+
+const Message = ({message, name}: MessageProps) => {
+  const {user, text} = message;
+  const trimmedName = name.trim().toLowerCase();
+  console.log(user, "and", trimmedName);
+  if (user === trimmedName) {
+    return <div className="messageContainer justifyEnd">
+      <p className="sentText pr-10">{name}</p>
+      <div className="messageBox backgroundBlue">
+        <p className="messageText colorWhite">{text}</p>
+      </div>
+    </div>
+  }
+
+  return <div className="messageContainer justifyStart">
+    <div className="messageBox backgroundLight">
+      <p className="messageText colorDark">{text}</p>
+    </div>
+    <p className="sentText pr-10">{user}</p>
+  </div>
+}
+
+export default Message;
