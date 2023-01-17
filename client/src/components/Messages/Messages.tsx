1f84462eefe615e2f3283d9cc7cffe24b47620e0
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-17 19:39:23 +0700

    feat: add `Message` component

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -2,12 +2,13 @@ import ScrollToBottom from "react-scroll-to-bottom";
 
 import "./Messages.css";
 import {MessagesProps} from "../../helper/interfaces";
+import Message from "../Message/Message";
 
 const Messages = ({messages, name}: MessagesProps) => {
   return <ScrollToBottom>
     {messages.map((message, index) =>
       <div key={index}>
-        {message} - {name}
+        <Message message={message} name={name} />
       </div>
     )}
   </ScrollToBottom>
