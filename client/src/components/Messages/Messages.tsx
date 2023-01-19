15b745871a9142a68c3a303d83e0947029d78862
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-19 19:05:31 +0700

    feat: add className `messages`

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -5,7 +5,7 @@ import {MessagesProps} from "../../helper/interfaces";
 import Message from "../Message/Message";
 
 const Messages = ({messages, name}: MessagesProps) => {
-  return <ScrollToBottom>
+  return <ScrollToBottom className='messages'>
     {messages.map((message, index) =>
       <div key={index}>
         <Message message={message} name={name} />
