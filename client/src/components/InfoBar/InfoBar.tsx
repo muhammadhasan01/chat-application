530453211aeac56e778e8c7167831181763d9377
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-20 16:29:35 +0700

    refactor: remove image word on restyle

--- client/src/components/InfoBar/InfoBar.tsx
+++ client/src/components/InfoBar/InfoBar.tsx
@@ -6,12 +6,12 @@ import {InfoBarProps} from "../../helper/interfaces";
 const InfoBar = ({ room }: InfoBarProps) => {
   return <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online image"/>
+      <img className="onlineIcon" src={onlineIcon} alt="online"/>
       <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
       <a href="/">
-        <img src={closeIcon} alt="close image"/>
+        <img src={closeIcon} alt="close"/>
       </a>
     </div>
   </div>
