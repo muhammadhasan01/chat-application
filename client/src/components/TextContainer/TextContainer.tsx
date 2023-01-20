ad60e77809d9a5d34705a50e2ecd898bc65c78fd
Muhammad Hasan <muhammadhasan50@gmail.com>
2023-01-20 16:28:39 +0700

    feat: add `TextContainer.tsx`

---
+++ client/src/components/TextContainer/TextContainer.tsx
@@ -0,0 +1,33 @@
+import OnlineIcon from "../../icons/onlineIcon.png";
+import {TextContainerProps} from "../../helper/interfaces";
+
+import "./TextContainer.css";
+
+const TextContainer = ({users}: TextContainerProps) => {
+  console.log("The length is " + users);
+  console.log("Wow", users.length);
+  return <div className="textContainer">
+    <div>
+      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+    </div>
+    {
+      users.length > 0 ? (
+        <div>
+          <h1>People currently chatting:</h1>
+          <div className="activeContainer">
+            <h2>
+              {users.map(name => (
+                <div key={name} className="activeItem">
+                  <img alt="Online Icon" src={OnlineIcon}/>
+                  {name}
+                </div>)
+              )}
+            </h2>
+          </div>
+        </div>
+      ) : null
+    }
+  </div>
+}
+
+export default TextContainer;
